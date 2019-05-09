a0559999ba73c1529c19ffc4e51640754afff012
Michael Jenkins <mjenkins9605@vidangel.com>
2019-05-09 15:05:36 -0400

    fixed get for ID - complete MVP

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ server.get("/api/users", (req, res) => {
     });
 });
 
+
 server.post("/api/users", (req, res) => {
   const user = req.body;
   db.insert(user)
@@ -38,47 +39,62 @@ server.post("/api/users", (req, res) => {
     });
 });
 
-server.get("/api/users:id", (req, res) => {
+
+server.get("/api/users/:id", (req, res) => {
   const userId = req.params.id;
   db.findById(userId)
-    .then(users => {
-      res.json(users);
-    })
-    .catch(err => {
-      res
-        .status(404)
-        .json({
+    .then(user => {
+      if (user) {
+        db.findById(userId).then(findUser => {
+          res.status(201).json(findUser);
+        });
+      } else {
+        res.status(404).json({
           error: err,
           message: "The user with the specified ID does not exist."
         });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: err,
+        message: "The user information could not be retrieved."
+      });
     });
 });
 
 
 server.delete("/api/users/:id", (req, res) => {
-    const id = req.params.id;
-    db.remove(id)
+  const id = req.params.id;
+  db.remove(id)
     .then(deleted => {
-        res.status(201).end();
+      res.status(201).end();
     })
     .catch(error => {
-        res.status(404).json({ message: "The user with the specified ID does not exist."});
+      res
+        .status(404)
+        .json({ message: "The user with the specified ID does not exist." });
     });
 });
 
+
 server.put("/api/users/:id", (req, res) => {
-    const { id } = req.params;
-    const changes = req.body;
-    db.update(id, changes)
+  const { id } = req.params;
+  const changes = req.body;
+  db.update(id, changes)
     .then(updated => {
-        if (updated) {
-            res.status(200).json(updated);
-        } else {
-            res.status(400).json({ message: "The user with the specified ID does not exist"});
-        }
+      if (updated) {
+        res.status(200).json(updated);
+      } else {
+        res
+          .status(400)
+          .json({ message: "The user with the specified ID does not exist" });
+      }
     })
     .catch(error => {
-        res.status(500).json({ message: "The user information could not be modified."});
+      res
+        .status(500)
+        .json({ message: "The user information could not be modified." });
     });
 });
 
